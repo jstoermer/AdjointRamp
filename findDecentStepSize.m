29f74e195cc8672032c298eb7667403dbaa0180f
Samitha Samaranayake <samitha@gmail.com>
2012-08-29 15:12:56 +0200

    update gradient decent files

--- findDecentStepSize.m
+++ findDecentStepSize.m
@@ -1,8 +1,8 @@
-function [alpha] = findDecentStepSize()
+function [stepSize] = findDecentStepSize(params)
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-alpha=1;
+stepSize=params.alpha*ones(params.T*(params.N-1));
 
 end
 
--- gradDecent.m
+++ gradDecent.m
@@ -1,7 +1,10 @@
-function [ output_args ] = untitled( input_args )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+function [u_updated] = gradDecent(u, partialJ_u, partialH5_u, lambda, params)
+% Given the current control u and the parameters required for gradient
+% decent, computes the new updated value of u 
 
+lambda5 = extractLambda5(lambda, params);
+gradJ_u = partialJ_u + lambda5'.*partialH5_u;
+alpha = findDecentStepSize();
+u_updated = u - alpha*gradJ_u;
 
 end
-
