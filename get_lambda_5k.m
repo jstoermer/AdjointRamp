d981cdeb6c68533d8bfb4e4927553c28dafad19b
Jack Reilly <jackdreilly@gmail.com>
2012-08-30 10:46:42 +0200

    fixed bug

--- get_lambda_5k.m
+++ get_lambda_5k.m
@@ -1,6 +1,6 @@
 function out = get_lambda_5k(scen, lambda, k)
 N = scen.N;
-start = idx_lookup(k, 'd', 1);
-endi = idx_lookup(k, 'd', N - 1);
+start = idx_lookup(k, 'd', 1,scen.N);
+endi = idx_lookup(k, 'd', N - 1,scen.N);
 out = lambda(start:endi);
 end
