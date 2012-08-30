f337865b05417d25abb543e7e186b3dbbfbc17eb
Samitha Samaranayake <samitha@gmail.com>
2012-08-30 10:59:04 +0200

    gradDecent minor update

--- gradDecent.m
+++ gradDecent.m
@@ -3,7 +3,7 @@ function [u_updated] = gradDecent(u, partialJ_u, diagOfPartialH5_u, lambda, scen
 % decent, computes the new updated value of u 
 
 lambda5 = extractConstraint5(lambda, scen);
-gradJ_u = partialJ_u + lambda5(1:scen.T*(scen.N-1)).*diagOfPartialH5_u;
+gradJ_u = partialJ_u + lambda5.*diagOfPartialH5_u;
 alpha = findDecentStepSize(scen);
 u_vect = reshape(u',1,scen.T*(scen.N-1));
 u_updated = u_vect - alpha.*gradJ_u;
