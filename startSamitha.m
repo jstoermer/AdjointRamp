5eca152061b3e69275d93efe14dec107a26de49b
Samitha Samaranayake <samitha@gmail.com>
2012-08-30 14:35:26 +0200

    add test files

--- startSamitha.m
+++ startSamitha.m
@@ -13,11 +13,11 @@ p0_vec = [0,0,0];
 l0_vec = [0,0,0];
 
 D0_vec = [1,0.8,0.8,0,0,0,0,0,0];
-D = [.5;.5;.5;0;0;0;0;0;0];
+D = [.5,0;.5,0;.5,0;0,0;0,0;0,0;0,0;0,0;0,0];
 
-u = [.3;.3;.3;.3;.3;.3;.3;.3;.3];
+u = 1.5*ones(9,2);
 
-beta = [0;0;0;0;0;0;0;0;0];
+beta = zeros(9,2);
 
 
 scen = scenario(p0_vec,l0_vec,D0_vec, D, cells, beta, dt, dx);
@@ -26,4 +26,4 @@ scen.u = u;
 % plot_states(scen.states);
 % s = adjoint_sln(scen, scen.states)
 
-adjoint_loop(scen,u);
+adjoint_loopSamitha(scen,u);
