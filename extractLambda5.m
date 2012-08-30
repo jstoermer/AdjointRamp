6465969fbdb2ecfc21e82845b4caf56c031d8bdc
Samitha Samaranayake <samitha@gmail.com>
2012-08-30 10:43:59 +0200

    modify extractLambda

--- extractLambda5.m
+++ extractLambda5.m
@@ -1,10 +1,11 @@
-function [ lambda5 ] = extractConstraint5(lambda, params)
+function [ lambda5 ] = extractLambda5(lambda, scen)
 % Given the adjoint variable vector lambda, extracts only the variables
 % corresponding to variable 5
 
-lambda5 = lambda(params.N*4+1:params.N*5);
-for i=1:params.T
-    lambda5 = lambda5 + lambda((i-1)*params.C*params.N + params.N*4+1: (i-1)*params.C*params.N + params.N*5)
+lambda5 = [];
+for k=0:scen.T-1
+    % get_lambda() returns a vector of length N-1
+    lambda5 = [lambda5 get_lambda_5k(scen, lambda, k)];
 end
 
 
--- updateControl.m
+++ updateControl.m
@@ -1,4 +1,4 @@
-function [ u_updated ] = updateControl(u, l, lambda, params)
+function [ u_updated ] = updateControl(u, l, lambda, scen)
 %UNTITLED9 Summary of this function goes here
 %   Detailed explanation goes here
 % params = struct('N', 'T', 'C', 'R', 'alpha', 'iter');
@@ -6,7 +6,7 @@ function [ u_updated ] = updateControl(u, l, lambda, params)
 partialJ_u = computePartialJ_u(R,u,l);
 diagOfPartialH5_u = computePartialH5_u(u,l);
 % Preform a single step of gradient decent
-u_updated = gradDecent(u, partialJ_u, diagOfPartialH5_u, lambda, params);
+u_updated = gradDecent(u, partialJ_u, diagOfPartialH5_u, lambda, scen);
 
 end
 
