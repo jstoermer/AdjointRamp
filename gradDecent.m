2f7081bc7478504b1b817e139c6c959aca185334
Samitha Samaranayake <samitha@gmail.com>
2012-08-30 11:07:29 +0200

    remove negative u

--- gradDecent.m
+++ gradDecent.m
@@ -2,10 +2,11 @@ function [u_updated] = gradDecent(u, partialJ_u, diagOfPartialH5_u, lambda, scen
 % Given the current control u and the parameters required for gradient
 % decent, computes the new updated value of u 
 
-lambda5 = extractLambda5(lambda, scen);
+lambda5 = extractLambda5(lambda, scen)';
 gradJ_u = partialJ_u + lambda5.*diagOfPartialH5_u;
 alpha = findDecentStepSize(scen);
 u_vect = reshape(u',1,scen.T*(scen.N-1));
 u_updated = u_vect - alpha.*gradJ_u;
 u_updated = reshape(u_updated', scen.T, scen.N-1);
+u_updated = max(u_updated,0);
 end
