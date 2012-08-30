1b5456cbc330a658c67b937ca82460a4f3236f6e
Jack Reilly <jackdreilly@gmail.com>
2012-08-30 19:31:44 +0200

    check out the two_onramp file, it has a successful demo

--- adjoint_loop.m
+++ adjoint_loop.m
@@ -1,12 +1,11 @@
 function u = adjoint_loop(scen,u0)
 u = u0;
-iter = 30;
+iter = 10;
 counter = 0;
-vals = []
+vals = [];
 us = [];
-tic
 while counter < iter;
-    counter;
+    counter
     us = [us, u(:)];
     scen.iter = counter + 1;
     counter = counter + 1;
@@ -19,8 +18,5 @@ while counter < iter;
     l = cell2mat(l(2:end)');
     u = updateControl(u, l, lambda, scen);
 end
-toc
 plt(1:length(vals),vals);
-% plt(1:length(us),us);
-
 end
