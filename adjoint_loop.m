eaf1cf28d4a7d34786fdeb52f705d1bbbd0b8c10
Jack Reilly <jackdreilly@gmail.com>
2012-09-04 15:49:57 +0200

    moved l and u computations inside of updatecontrol

--- adjoint_loop.m
+++ adjoint_loop.m
@@ -14,9 +14,7 @@ while counter < iter;
     [v1,v2,v3,v4] = cost_function(scen);
     vals = [vals, [v1,v2,v3,v4]'];
     lambda = adjoint_sln(scen,scen.states);
-    l = {scen.states.ramp_queues};
-    l = cell2mat(l(2:end)');
-    u = updateControl(u, l, lambda, scen);
+    u = updateControl(lambda, scen);
 end
 plt(1:length(vals),vals);
 end
