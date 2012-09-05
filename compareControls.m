a72bfe7123b22fd76a5339ee089a521d23466f17
Jack Reilly <jackdreilly@gmail.com>
2012-09-05 10:29:34 +0200

    bug

--- compareControls.m
+++ compareControls.m
@@ -1,8 +1,10 @@
 function compareControls(scen, u1, u2)
 scen1 = scen;
+scen1.u = u1;
 scen2 = scen;
-scen1 = forward_sim(scen1,u1);
-scen2 = forward_sim(scen2, u2);
+scen2.u = u2;
+scen1 = forward_sim(scen1);
+scen2 = forward_sim(scen2);
 
 plotCompareScenarios(scen1, scen2);
 
