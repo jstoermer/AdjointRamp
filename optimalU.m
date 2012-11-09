e9d7522d6d3a8e9ed067f8faf5d921799a9a8860
Samitha Samaranayake <samitha@gmail.com>
2012-11-08 17:17:12 -0800

    updating optimalU

--- optimalU.m
+++ optimalU.m
@@ -1,4 +1,12 @@
 function [u, outputState] = optimalU(varargin)
+% Computes the optimal ramp metering control for a given scenario
+% Inputs
+% varargin(1) - JSON description of a scenario or the scenario structure
+% varargin(2) - (optional) Initial control
+% Outputs
+% u - Optimal control 
+% outputState - State evolution resulting from the optimal control
+
 scen = jsonOrScen(varargin{1});
 try
     u = varargin{2};
@@ -7,16 +15,19 @@ catch e
     u = chooseInitialU(scen);
 end
 
+iteration = 0;
 while true
+    iteration = iteration + 1;
     outputState = forwardSimulation(scen, u);
     gradient = gradientRampControl(scen, outputState, u);
     nextU = nextRampControl(scen, gradient, u);
-    if stopIterating(scen, u, nextU)
+    if stopIterating(scen, u, nextU, iteration)
         u = nextU;
         return;
     end
     u = nextU;
 end
+
 end
 
 
@@ -27,7 +38,17 @@ function gradient = gradientRampControl(scen, outputState, u)
 end
 
 function nextU = nextRampControl(scen, gradient, u)
+% Updates the control given a scenario, current control and gradient
+switch globalDescentAlgorithm
+    case 'basicGradientDescent'
+        basicGD(scen, gradient, u);
+    case 'backtrackingLineSearch'
+        btLineSearch(scen, gradient, u);
+    otherwise
+        warning('Unknown descent algorithm');
+end
 end
 
-function stop = stopIterating(scen, u, nextU)
+function stop = stopIterating(scen, u, nextU, iteration)
+    stop = (nextU - u < globalConvergenceThreshold) | (iteration > globalMaxIterations);
 end
