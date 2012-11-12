e901f38326b06221c56929587419790c3aee43f3
Jack Reilly <jackdreilly@gmail.com>
2012-11-12 14:48:09 -0800

    removed field from optimalU

--- plotBeforeAndAfter.m
+++ plotBeforeAndAfter.m
@@ -1,14 +1,10 @@
-function plotBeforeAndAfter(varargin)
-fn = varargin{1};
-if nargin == 2
-    useBFGS = varargin{2};
-else
-    useBFGS = true;
-end
-s1 = loadJSONScenario(fn);
+function plotBeforeAndAfter(fn)
+s1 = loadScenario(fn);
+% TODO: add function that allows for either some control or no control if
+% no u
 plotForwardSim(s1);
-s2 = loadJSONScenario(fn);
-optimalU = getOptimalMetering(s2,useBFGS);
+s2 = loadScenario(fn);
+optimalU = optimalU(s2);
 s2.u = optimalU;
 plotForwardSim(s2);
 plotSimDifference(s1,s2);
