8b490cb4726bd0491210db33849f1e326a53a7ac
Jack Reilly <jackdreilly@gmail.com>
2012-11-12 16:34:15 -0800

    better diff plot

--- plotSimDifference.m
+++ plotSimDifference.m
@@ -12,6 +12,8 @@ sumr1 = sum(rho1,1);
 sumr2 = sum(rho2,1);
 suml1 = sum(l1,1);
 suml2 = sum(l2,1);
+tot1 = sumr1 + suml1;
+tot2 = sumr2 + suml2;
 figure
 h = area(cumsum([sumr1;suml1],2)');
 set(h(1),'FaceColor',[.5 0 0])
@@ -27,12 +29,13 @@ title('Comparative Density');
 alpha(.4);
 legend('prev main', 'prev ramp', 'opt main', 'opt ramp');
 figure;
-h = area(cumsum([sumr2;suml2],2)'-cumsum([sumr1;suml1],2)');
-set(h(1),'FaceColor',[1.0 0.0 0])
-set(h(2),'FaceColor',[0 1.0 0])
-alpha(.4);
-legend('density diff', 'ramp diff');
+grid on;
+hold on;
+plot(cumsum(sumr2 - sumr1),'b-.','LineWidth',2);
+plot(cumsum(suml2 - suml1),'r:','LineWidth', 2);
+plot(cumsum(tot2 - tot1),'k', 'LineWidth', 3);
+legend('density diff', 'ramp diff', 'tot diff');
 xlabel('Time');
-ylabel('Cumulative Density');
-title('Cumulative Density Difference');
+ylabel('Cum. Density');
+title('Cum .Density Difference');
 end
