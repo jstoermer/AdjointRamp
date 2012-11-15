e027e7394209c9b182b91baad36bd7d0b44d76d9
Jack Reilly <jackdreilly@gmail.com>
2012-11-15 09:46:48 -0800

    some more sweet test cases to make sure forward sim is working correctly

--- tests/ForwardSimTestCase.m
+++ tests/ForwardSimTestCase.m
@@ -29,6 +29,41 @@ classdef ForwardSimTestCase < TestCase
       assertElementsAlmostEqual(carsLeft, carsIn - carsOut);
     end
     
+    function testFluxConsistency(self)
+      os = self.states;
+      fout = [zeros(size(os.fluxOut,1),1) os.fluxOut];
+      foff = [zeros(size(os.fluxOffRamp,1),1) os.fluxOffRamp];
+      fin = [os.fluxIn zeros(size(os.fluxOut,1),1)];
+      framp = [os.fluxRamp zeros(size(os.fluxOut,1),1)];
+      assertVectorsAlmostEqual(fout + framp,  fin + foff);
+    end
+    
+    function testQueueBalance(self)
+      os = self.states; scen = self.scen;
+      l = os.queue;
+      
+      nextQueue = l(2:end, :); 
+      prevQueue = l(1:end-1, :);
+      carsIn = scen.BC.D .* scen.dt;
+      carsOut = os.fluxRamp .* scen.dt;
+      
+      assertVectorsAlmostEqual(nextQueue, prevQueue + carsIn - carsOut);
+    end
+    
+    function testLinkBalance(self)
+      os = self.states; scen = self.scen;
+      rho = os.density;
+      L = [scen.links.L];
+      
+      nextCars = rho(2:end, :).*repmat(L, scen.T, 1); 
+      prevCars = rho(1:end-1, :).*repmat(L, scen.T, 1); 
+      carsIn = os.fluxIn .* scen.dt;
+      carsOut = os.fluxOut .* scen.dt;
+      
+      assertVectorsAlmostEqual(nextCars, prevCars + carsIn - carsOut);
+    end
+    
+    
     function tearDown(self)
     end
   end
