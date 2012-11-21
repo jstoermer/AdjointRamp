bb63716f8aa58e94d5063c55799489e5514085af
Jack Reilly <jackdreilly@gmail.com>
2012-11-21 09:47:47 -0800

    new big example

--- two_onramp_two_offramp_demo.m
+++ two_onramp_two_offramp_demo.m
@@ -8,21 +8,20 @@ v_vec = ones(1,N);
 w_vec = ones(1,N);
 fm_vec = ones(1,N);
 
-R = 1;
+
 p = 0.8*ones(1,N);
 
-p0_vec = zeros(T,N);
-l0_vec = zeros(T,N);
-D0_vec = zeros(1,T);
-D = zeros(T,N-1);
+p0_vec = zeros(1,N);
+l0_vec = zeros(1,N);
+D = zeros(T,N);
 
-D0_vec(1) = N*1;
+D(1) = N*1;
 D(N-1,N-1) = N*2;
 
 u = ones(T, N-1) * .2;
 
 
-beta = zeros(T,N-1);
+beta = zeros(T,N);
 beta(:,N-2) = .1;
 
 %% half offramp and onramp
@@ -31,34 +30,18 @@ half = ceil(N/2);
 D(half,half) = N*2;
 beta(:,half - 1) = .1;
 
+rmax = ones(1,N);
 
+scen.dt = dt;
+scen.l0 = l0_vec;
+scen.L = l_vec;
+scen.v = v_vec;
+scen.w = w_vec;
+scen.fm = fm_vec;
+scen.p = p;
+scen.p0 = p0_vec;
+scen.D = D;
+scen.beta = beta;
+scen.rmax = rmax;
 
-
-%% create cells
-
-
-for i = 1:N
-    cells(i) = highway_cell(v_vec(i), w_vec(i), fm_vec(i), l_vec(i));
-end
-
-
-
-%% scenario 1
-scen = scenario(p0_vec,l0_vec,D0_vec, D, cells, beta, p, dt, dx);
-scen.R = R;
-
-
-%% special u
-
-% u(1:N*3,:) = .7;
-
-% scen.u = u;
-
-% adjoint_loop(scen, u);
-% final_u = adjointBFGS(scen,u);
-% compareControlPlot(scen, u);
-scen = mpcRamp(scen, u, 1,1);
-
-% scen = forward_sim(scen);
-% plot_states(scen.states);
-% disp(cost_function(scen));
+savejson('',scen, 'networks/2on2off.json')
