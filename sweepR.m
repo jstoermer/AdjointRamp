7a2a635926ab81d60c07a75398e9ee058741bdca
Josh Stoermer <jstoermer@berkeley.edu>
2013-01-22 10:53:09 -0800

    Renamed previous sweepR to sweepRvsTime. Created new sweepR.

--- sweepR.m
+++ sweepR.m
@@ -1,26 +1,33 @@
-function [] = sweepR(scenFile, numTrials)
+function [] = sweepR(scenFile, initR, numTrials)
 
-% The last argument will always be the number of trials. Trials correspond
-% to the following values for R: 0.01, 0.1, 1, 10, 100, ...
+% initR is the starting value for r. For each trial, r is multiplied by 10.
+% e.g. initR = 0.01, numTrials = 3 gives trials with r = 0.01, 0.1, 1.
 
-scen = io.loadScenario(scenFile);
-
-t = zeros(1, numTrials);
 r = zeros(1, numTrials);
+cost = zeros(1, numTrials);
 
 global parameters;
+scen = io.loadScenario(scenFile);
+u0 = chooseInitialU(scen);
+descent = parameters.globalDescentAlgorithm;
+s = rampAdjointStructures(scen);
 
 for i = 1:numTrials
-    parameters.R = 10^(i-3);
+    parameters.R = initR * 10^(i - 1);
+    [u, totCost] = adjointOptimization(scen, stacker(u0), s.state, s.cost, s.dhdx, s.djdx, s.dhdu, s.djdu, descent);
     r(i) = parameters.R;
-    tic;
-    rampOptimalU(scen);
-    t(i) = toc;
+    
+    % Do we want the sum of the costs?
+    cost(i) = sum(totCost);
+    
+    % Or the final cost?
+    % cost(i) = totCost(end);
+    
 end % end for i = 1:numTrials
 
-plot(r, t);
-title('Algorithm Time vs. R-Value');
-xlabel('R-Value');
-ylabel('Algorithm Time (Seconds)');
+semilogx(r, cost);
+title('Cost vs. \it{r}\rm-Value');
+xlabel('\it{r}\rm-Value');
+ylabel('Cost');
 
 end % end sweepR
---
+++ sweepRvsTime.m
@@ -0,0 +1,26 @@
+function [] = sweepRvsTime(scenFile, numTrials)
+
+% The last argument will always be the number of trials. Trials correspond
+% to the following values for R: 0.01, 0.1, 1, 10, 100, ...
+
+scen = io.loadScenario(scenFile);
+
+t = zeros(1, numTrials);
+r = zeros(1, numTrials);
+
+global parameters;
+
+for i = 1:numTrials
+    parameters.R = 10^(i-3);
+    r(i) = parameters.R;
+    tic;
+    rampOptimalU(scen);
+    t(i) = toc;
+end % end for i = 1:numTrials
+
+plot(r, t);
+title('Algorithm Time vs. R-Value');
+xlabel('R-Value');
+ylabel('Algorithm Time (Seconds)');
+
+end % end sweepRvsTime
