48236489107a968208208bf4f903c7c03429e33c
Josh Stoermer <jstoermer@berkeley.edu>
2013-01-22 09:18:55 -0800

    Completed rampOptimalUPlot.m.

--- rampOptimalUPlot.m
+++ rampOptimalUPlot.m
@@ -2,11 +2,13 @@ function [] = rampOptimalUPlot(varargin)
 
 scen = varargin{1};
 
-if (length(varargin) > 1)
-    initControl = varargin{2};
-    
+if (length(varargin) == 1)
+    rampOptimalU(scen, true);
+elseif (length(varargin) == 2)
+    initCont = varargin{2};
+    rampOptimalU(scen, initCont, true);
+else
+    error('Incorrect number of arguments. rampOptimalUPlot can have at most two arguments, e.g. a scenario and an initial control.');
 end % end if
-
-rampOptimalU(varargin);
 
 end % end rampOptimalUPlot
