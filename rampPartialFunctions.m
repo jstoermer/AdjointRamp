73194289966a5bce6cf9595ec1054b21829b4d21
Josh Stoermer <jstoermer@berkeley.edu>
2013-02-19 17:21:20 -0800

    Fixed changes that were made during the merge.

--- rampPartialFunctions.m
+++ rampPartialFunctions.m
@@ -11,18 +11,18 @@ function out = dh_du(scen, states, u)
 T = scen.T; N = scen.N;
 out = sparse(N*(T+1)*8, N*T);
 for k = 1:T
-  for i = 1:N
-    l = states.queue(k,i);
-    rmax = scen.links(i).rmax;
-    u_cur = u(k,i);
-    if u_cur < min(l / scen.dt, rmax)
-      out(8*N*(k - 1) + N*(5 - 1) + i, N*(k - 1) + i) = -1;
+    for i = 1:N
+        l = states.queue(k,i);
+        rmax = scen.links(i).rmax;
+        u_cur = u(k,i);
+        if u_cur < min(l / scen.dt, rmax)
+            out(8*N*(k - 1) + N*(5 - 1) + i, N*(k - 1) + i) = -1;
+        end
     end
-  end
 end
 end
 
-function out = dh_dx(scen, states, u)
+function varargout = dh_dx(scen, states, u)
 
 dt = scen.dt;
 T = scen.T;
@@ -49,13 +49,13 @@ supply = states.supply;
 demand = states.demand;
 fluxIn = states.fluxIn;
 for i = 1:N
-  vmat(i) = scen.links(i).v;
-  Fmat(i) = scen.links(i).fm;
-  pmmat(i) = scen.links(i).pm;
-  pmat(i) = scen.links(i).p;
-  wmat(i) = scen.links(i).w;
-  Lmat(i) = scen.links(i).L;
-  rmaxmat(i) = scen.links(i).rmax;
+    vmat(i) = scen.links(i).v;
+    Fmat(i) = scen.links(i).fm;
+    pmmat(i) = scen.links(i).pm;
+    pmat(i) = scen.links(i).p;
+    wmat(i) = scen.links(i).w;
+    Lmat(i) = scen.links(i).L;
+    rmaxmat(i) = scen.links(i).rmax;
 end
 out = sparse(n,n);
 hi = 0;
@@ -81,7 +81,7 @@ for k = 1:T+1 % time step iterator
             colInd(indexCount) = hi;
             sparseVal(indexCount) = 1;
             switch ci
-                case 1; rhoConstraints();    
+                case 1; rhoConstraints();
                 case 2; lConstraints();
                 case 3; delConstraints();
                 case 4; sigConstraints();
@@ -105,9 +105,9 @@ forToc = toc(forTic);
         colInd(indexCount) = N*8*(k - 1 - 1) + N*(1 -1) + i;
         sparseVal(indexCount) = -1;
         
-    l = Lmat(i);
+        l = Lmat(i);
         
-    out(hi,N*8*(k - 1 - 1) + N*(7 -1) + i) = dt / l;
+        out(hi,N*8*(k - 1 - 1) + N*(7 -1) + i) = dt / l;
         %out(hi,N*8*(k - 1 - 1) + N*(6 -1) + i) = -dt / l;
         indexCount = indexCount + 1;
         rowInd(indexCount) = hi;
@@ -120,16 +120,13 @@ forToc = toc(forTic);
         colInd(indexCount) = N*8*(k - 1 - 1) + N*(7 -1) + i;
         sparseVal(indexCount) = dt / l;
         
-  end
-  function lConstraints()    
-    if k == 1; return; end;
         rhoConstrToc = [rhoConstrToc, toc(t1)];
     end
     function lConstraints()
         t2 = tic;
         if k == 1; return; end;
         
-    out(hi,N*8*(k - 1 - 1) + N*(8 -1) + i) = dt;
+        out(hi,N*8*(k - 1 - 1) + N*(8 -1) + i) = dt;
         %out(hi,N*8*(k - 1 - 1) + N*(2 -1) + i) = -1;
         indexCount = indexCount + 1;
         rowInd(indexCount) = hi;
@@ -142,13 +139,6 @@ forToc = toc(forTic);
         colInd(indexCount) = N*8*(k - 1 - 1) + N*(8 -1) + i;
         sparseVal(indexCount) = dt;
         
-  end
-  function delConstraints()
-    rho = density(k,i);
-    v = vmat(i);
-    F = Fmat(i);
-    if rho * v < F
-      out(hi,N*8*(k - 1) + N*(1 -1) + i) = -v;
         lConstrToc = [lConstrToc, toc(t2)];
     end
     function delConstraints()
@@ -253,11 +243,11 @@ forToc = toc(forTic);
             beta = betamat(kk,i+1);
             d = rampDemand(kk,i+1);
             p = pmat(i+1);
-      sig = supply(kk,i+1);
+            sig = supply(kk,i+1);
             if del*beta + d <= sig || fin * p / beta >= del
-        out(hi, N*8*(k - 1) + N*(3 -1) + i) = -1;
-      elseif fin * (1 - p) >= d
-        out(hi,N*8*(k - 1) + N*(6 -1) + i+1) = - 1 / beta;
+                %out(hi, N*8*(k - 1) + N*(3 -1) + i) = -1;
+                indexCount = indexCount + 1;
+                rowInd(indexCount) = hi;
                 colInd(indexCount) = N*8*(k - 1) + N*(3 -1) + i;
                 sparseVal(indexCount) = -1;
             elseif fin * (1 - p) >= d
@@ -343,10 +333,10 @@ R = parameters.R;
 
 
 out = sparse(...
-  stacker(...
-  R.*(max(u -...
-  min(repmat([scen.links.rmax], scen.T, 1),...
-  states.queue(1:end-1,:)./scen.dt),0)))');
+    stacker(...
+    R.*(max(u -...
+    min(repmat([scen.links.rmax], scen.T, 1),...
+    states.queue(1:end-1,:)./scen.dt),0)))');
 end
 
 function out = dJ_du_barrier(scen, states, u)
@@ -360,8 +350,8 @@ barrierSum = barrierMaxGrad(u, .1 + min(rMax, states.queue(1:end-1,:)));
 % barrierSum = barrierSum + barrierMinGrad(u, 0);
 
 out = sparse(...
-  stacker(...
-  R.*(barrierSum))');
+    stacker(...
+    R.*(barrierSum))');
 end
 
 function out = dJ_dx(scen, ~, ~)
@@ -369,16 +359,13 @@ T = scen.T; N = scen.N;
 n = scen.nConstraints;
 out = sparse(1,n);
 
-for k = 1:T+1   
-  for i = 1:N
-    out(idx(N,k,1,i)) = scen.links(i).L * scen.dt;
-  end
-  for i = 1:N
-    out(idx(N,k,2,i)) = scen.dt;
-  end
+for k = 1:T+1
+    for i = 1:N
+        out(idx(N,k,1,i)) = scen.links(i).L * scen.dt;
+    end
+    for i = 1:N
+        out(idx(N,k,2,i)) = scen.dt;
+    end
 end
 
-
 end
-
-
