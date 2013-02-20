d5bf2f85e766d4d4b984dc303ab2cefc76dae302
Josh Stoermer <jstoermer@berkeley.edu>
2013-02-19 17:24:05 -0800

    Changed the output arguments for morphU.m.

--- morphU.m
+++ morphU.m
@@ -2,9 +2,16 @@
     % take a function which requires unstacked u, and return a function
     % which takes stacked u, and feeds the unstacked version to the guy
     % underneath
-    function out2 = helper(varargin)
+    function varargout = helper(varargin)
       varargin{upos} = unstack(varargin{upos},scen);
-      out2 = fn(varargin{:});
+      if strcmp(func2str(fn), 'dh_dx')
+          [out2, out3] = fn(varargin{:});
+          varargout{1} = out2;
+          varargout{2} = out3;
+      else
+          out2 = fn(varargin{:});
+          varargout{1} = out2;
+      end % if
     end
   out = @helper;
   end
